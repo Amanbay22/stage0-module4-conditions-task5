5b84122b602246a39d93045cac2345a09a37845c
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2022-08-08 20:54:11 +0600

    first try

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year % 400 == 0) {
+            System.out.println("leap");
+        }
+        // not a leap year if divisible by 100
+        // but not divisible by 400
+        else if (year % 100 == 0) {
+            System.out.println("not leap");
+        }
+        // leap year if not divisible by 100
+        // but divisible by 4
+        else if (year % 4 == 0) {
+            System.out.println("leap");
+        }
+        // all other years are not leap years
+        else {
+            System.out.println("not leap");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,34 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            // Cases for 31 Days
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
+
+            // Cases for 30 Days
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+
+            // Case for 28/29 Days
+            case 2:
+                System.out.println(28);
+                break;
+
+            default:
+                System.out.println("wrong number!");
+                break;
+        }
     }
 }
